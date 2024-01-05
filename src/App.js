e65635f8e54e03205bc40c20e1c3da798e6570fb
Adrian Bryant <bryant.adrian94@gmail.com>
2024-01-05 14:02:23 -0500

    working on map function to render data on the screen

--- src/App.js
+++ src/App.js
@@ -1,34 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-
+import { useEffect, useState } from 'react';
 
 
 
 function App() {
 
+  const [data, setData] = useState([]);
 
+
+
+  const fetchPokemon = () => {
     axios.get("https://pokeapi.co/api/v2/pokemon/ditto")
-    .then((response) => {
-        var result = response.data;
-        console.log(result);
+      .then((response) => {
+        const result = response.data;
+        console.log(result.abilities);
+        setData(result.abilities)
       },
-      (error) => {
-        console.log(error);
-      }
-    );
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
 
-
-
+  useEffect(() => {
+    fetchPokemon()
+  }, [])
 
 
   return (
     <div className="App">
-      
+
+
+
       <p>
         Edit <code>src/App.js</code> and save to reload.
       </p>
       <h1>PokeDex</h1>
+      {data.map((pokeObj) => {
+        return <p>  hp: {pokeObj.abilities}</p>
+      })}
       <p>
         <li>
           Hp:
